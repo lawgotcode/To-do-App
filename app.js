ca6d5c54738cb7561f38b8c663e3b8accf89e270
Lawrence Agyei <lawgotcode1990@gmail.com>
2017-10-03 11:44:48 -0700

    to do app

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var fs = require('fs');
+var http = require('http');
 
-fs.readFile('readMe.txt', 'utf8', function(err, data){
-	fs.writeFile('writeMe.txt', data);
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end('Hey Law');
 });
 
+server.listen(3000, '127.0.0.1');
+console.log('yo dawgs, now listening to hip hop');
